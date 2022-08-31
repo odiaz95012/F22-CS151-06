c4147984f9585557eee5aa3b0c47c4a0f323b8d9
odiaz95012 <odiaz95012@gmail.com>
2022-08-31 14:46:18 -0700

    Added DiazVegaTest.java to complete Q1 from HW1.

--- src/hw1/DiazVegaTest.java
+++ src/hw1/DiazVegaTest.java
@@ -1,10 +1,16 @@
 package hw1;
-
 public class DiazVegaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String name = "Oscar Diaz Vega";
+		String oscarsIntroduction = "Hello! My name is Oscar Diaz Vega. I am a third year computer science major."
+				+ "Some of my favorite things to do are play golf, play video games, and lift weights.";
+		int oscarsStudentID = 0610; // only the last 4 digits
+		
+		Student oscarDiazVega = new Student(name, oscarsIntroduction, oscarsStudentID);
+		
+		
 	}
 
 }
